e9d1f9fe1facaa5a75344359d59d77b482c3960d
Bruce Markham <219281+brucificus@users.noreply.github.com>
2020-03-15 20:42:54 -0400

    👷 Adds Rudimentary Build Process

---
+++ index.ts
@@ -0,0 +1,6 @@
+import { HTMLLexer } from "./antlr4ts_out/HTMLLexer"
+import { HTMLParser } from "./antlr4ts_out/HTMLParser"
+import { HTMLParserListener } from "./antlr4ts_out/HTMLParserListener"
+import { HTMLParserVisitor } from "./antlr4ts_out/HTMLParserVisitor"
+
+export { HTMLLexer, HTMLParser, HTMLParserListener, HTMLParserVisitor }
